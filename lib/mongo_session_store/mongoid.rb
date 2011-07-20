fe5c3a7a38f221ddc93252726338b44bfeeed3c8
Warlley Rezende <warlleyrezende@gmail.com>
2011-07-20 09:41:50 -0300

    Destroy session method

--- lib/mongo_session_store/mongoid.rb
+++ lib/mongo_session_store/mongoid.rb
@@ -17,6 +17,12 @@ module ActionDispatch
       self.session_class = Session
 
       SESSION_RECORD_KEY = 'rack.session.record'.freeze
+      
+      def destroy(env)
+        session = @@session_class.first(:conditions => { :_id => env[SESSION_RECORD_KEY].id })
+        session.destroy
+        env[SESSION_RECORD_KEY] = nil
+      end
 
       private
         def generate_sid
